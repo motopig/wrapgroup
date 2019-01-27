b27ac1ccea737cc09f01b2f237a1f6cd21edd551
moto <motoyet@gmail.com>
2019-01-27 22:04:08 +0800

    init

--- wrapgroup.go
+++ wrapgroup.go
@@ -1,1 +1,61 @@
 package wrapgroup
+
+import (
+	"context"
+	"sync"
+	"time"
+)
+
+const defaultNumber = 20
+const defaultTimeOut = 20
+
+type WrapGroup struct {
+	shrink chan struct{}  // 控制channel数量
+	timeout int  // 每个channel超时时间 second
+	wg      sync.WaitGroup
+}
+
+func Generate(number int, overtime int) WrapGroup {
+	num := defaultNumber
+	timeout := defaultTimeOut
+	if number > 0 {
+		num = number
+	}
+	if overtime > 0 {
+		timeout = overtime
+	}
+	return WrapGroup{
+		shrink: make(chan struct{}, num),
+		timeout: timeout,
+		wg:      sync.WaitGroup{},
+	}
+}
+
+func (w *WrapGroup) Add() {
+	err := w.AddWithTimeOut()
+	if err != nil {
+		//
+	}
+}
+
+func (w *WrapGroup) AddWithTimeOut() error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(w.timeout) * time.Second)
+	defer cancel()
+	select {
+	case <-ctx.Done():
+		return ctx.Err()
+	case w.shrink <- struct{}{}:
+		break
+	}
+	w.wg.Add(1)
+	return nil
+}
+
+func (w *WrapGroup) Done() {
+	<-w.shrink
+	w.wg.Done()
+}
+
+func (w *WrapGroup) Wait() {
+	w.wg.Wait()
+}
--- wrapgroup_test.go
+++ wrapgroup_test.go
@@ -1,1 +1,53 @@
 package wrapgroup
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/imroc/req"
+	"io"
+	"os"
+	"strings"
+	"testing"
+)
+func TestGenerate(t *testing.T) {
+	wg := Generate(50, 2)
+
+	f, err := os.Open("urls.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	rd := bufio.NewReader(f)
+	for {
+		line, err := rd.ReadString('\n')
+		if err != nil || io.EOF == err {
+			break
+		}
+		wg.Add()
+		go func(line string) {
+			defer wg.Done()
+			download(line)
+		}(line)
+	}
+	wg.Wait()
+}
+
+func download(pic string){
+	progress := func(current, total int64) {
+		fmt.Println(float32(current)/float32(total)*100, "%")
+	}
+	a := strings.Split(pic, "/")
+	al := len(a)
+
+	//req.SetTimeout(30 * time.Second)
+	r, e := req.Get(pic, req.DownloadProgress(progress))
+	if e == nil {
+		filPath := "./pics/" + a[al - 1]
+		fmt.Println(filPath)
+		r.ToFile(filPath)
+		fmt.Println("download complete")
+	} else {
+		fmt.Println("can not fetch the img")
+	}
+}
